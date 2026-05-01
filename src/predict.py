07ff1245b21fce0199b780b8f693110b329a712e
unknown <vardakunde38@gmail.com   git config --global user.name Varda>
2026-05-01 19:47:25 +0530

    Fixed model path for deployment

--- src/predict.py
+++ src/predict.py
@@ -1,8 +1,14 @@
+import os
 import pickle
 from src.preprocess import clean_text
 
-model = pickle.load(open("models/model.pkl", "rb"))
-vectorizer = pickle.load(open("models/vectorizer.pkl", "rb"))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+model_path = os.path.join(BASE_DIR, "models", "model.pkl")
+vectorizer_path = os.path.join(BASE_DIR, "models", "vectorizer.pkl")
+
+model = pickle.load(open(model_path, "rb"))
+vectorizer = pickle.load(open(vectorizer_path, "rb"))
 
 def predict_sentiment(text):
     cleaned = clean_text(text)
